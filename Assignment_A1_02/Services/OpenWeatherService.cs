a8b22ef3f2a977ba63a1abf0cb9b8b29275f89e3
StefanBrodin <42447271+StefanBrodin@users.noreply.github.com>
2026-01-12 21:59:10 +0100

    A1.2 OpenWeatherService.cs
    Första försöket till A1.2 versionen av OpenWeatherService.cs bestående av ny kod events + tidigare kod från A1.1. Dessutom nullable properties i ForecastItem.cs

--- Assignment_A1_02/Services/OpenWeatherService.cs
+++ Assignment_A1_02/Services/OpenWeatherService.cs
@@ -5,26 +5,30 @@ namespace Assignment_A1_02.Services;
 public class OpenWeatherService
 {
     readonly HttpClient _httpClient = new HttpClient();
-    readonly string _apiKey = "your_api_key_here"; // Replace with your OpenWeatherMap API key
+    readonly string _apiKey = "5c1873370d6530313e703ee2ce959255"; // Replace with your OpenWeatherMap API key
 
-    //Event declaration
+    // Event declaration
     public event EventHandler<string> WeatherForecastAvailable;
     protected virtual void OnWeatherForecastAvailable (string message)
     {
         WeatherForecastAvailable?.Invoke(this, message);
     }
-    public async Task<Forecast> GetForecastAsync(string City)
+
+    // Forecast by City
+    public async Task<Forecast> GetForecastAsync(string city)
     {
         //https://openweathermap.org/current
         var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
-        var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={_apiKey}";
+        var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&lang={language}&appid={_apiKey}";
 
         Forecast forecast = await ReadWebApiAsync(uri);
-        
-        //Event code here to fire the event
-        //Your code
+
+        // Event code here to fire the event
+        OnWeatherForecastAvailable($"New weather forecast for {city} available");
         return forecast;
     }
+
+    // Forecast by GeoLocation
     public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
     {
         //https://openweathermap.org/current
@@ -33,22 +37,39 @@ public class OpenWeatherService
 
         Forecast forecast = await ReadWebApiAsync(uri);
 
-        //Event code here to fire the event
-        //Your code
+        // Event code here to fire the event
+        OnWeatherForecastAvailable($"New weather forecast for ({latitude}, {longitude}) available");
         return forecast;
     }
+
     private async Task<Forecast> ReadWebApiAsync(string uri)
     {
         HttpResponseMessage response = await _httpClient.GetAsync(uri);
         response.EnsureSuccessStatusCode();
-        
-        //Convert Json to NewsResponse
+
+        // Convert Json to NewsResponse
         string content = await response.Content.ReadAsStringAsync();
         WeatherApiData wd = JsonConvert.DeserializeObject<WeatherApiData>(content);
 
-        //Convert WeatherApiData to Forecast using Linq.
-        //Your code
-        var forecast = new Forecast(); //dummy to compile, replaced by your own code
+        // Create the forecast object of type Forecast based on WeatherApiData.
+        // Now a bit more fail-safe by better handling of potentially missing data/null
+        var forecast = new Forecast
+        {
+            City = wd.city.name,
+            Items = wd.list
+                    .Select(item => new ForecastItem
+                    {
+                        DateTime = UnixTimeStampToDateTime(item.dt), // Can never be NULL after serialization, at worst it will be 0 which will mean 1970-01-01, an obvious error!
+                        Temperature = item.main?.temp ?? double.NaN,
+                        WindSpeed = item.wind?.speed ?? double.NaN,
+                        Description = item.weather.FirstOrDefault()?.description ?? "No Description!",
+                        Icon = item.weather.FirstOrDefault()?.icon is string icon
+                            ? $"http://openweathermap.org/img/w/{icon}.png"
+                            : null
+                    })
+                    .ToList()
+        };
+
         return forecast;
     }
 
